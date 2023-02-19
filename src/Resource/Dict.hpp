14f9af481dd07fae84a5abf9b28e62b34195eee1
DrEden33773 <edwardwang33773@gmail.com>
2023-02-20 01:34:05 +0800

    update

--- src/Resource/Dict.hpp
+++ src/Resource/Dict.hpp
@@ -11,17 +11,19 @@
 
 #pragma once
 
+#include <map>
 #include <memory>
 #include <string>
 #include <unordered_map>
 
 namespace Resource {
 
+using std::map;
 using std::shared_ptr;
 using std::string;
 using std::unordered_map;
 
-using dict_t = unordered_map<string, string>;
+using dict_t = map<string, string>;
 
 static const shared_ptr<dict_t> Dict = std::make_shared<dict_t>();
 
